bd7cbafc543922cdb092f085fe56715a2d95bd43
DimaSemchuk <dima.september@gmail.com>
2014-04-20 14:46:03 +0300

    Function for peer assessment
    Function commited

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code creates a matrix with length 0, gives it setters and getters.
+## THen makes an inverse if not yet made.
 
-## Write a short comment describing this function
+## This function takes numeric sqare matrix as a parameter and gives back getters and setters
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix(numeric(0), 0,0)) {
 
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
+  
 
 
-## Write a short comment describing this function
+
+## this part takes makeCacheMatrix's output as a parameter and calculates inversion if not at the place
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+      i <- x$getinverse()
+    if(length(i) > 0) {
+      message("getting cached data")
+      return(i)
+    }
+    data <- x$get(x)
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
+  }
